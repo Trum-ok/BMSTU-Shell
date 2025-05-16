4880681b38e667fd3615795558a089d3f318fedc
Trum <artamarkan@gmail.com>
2025-05-17 01:57:46 +0300

    update run script

--- run-ubuntu24.sh
+++ run-ubuntu24.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
-PATH_TO_DOCKERFILE="$HOME/VScProjects/BMSTU/BMSTU-Shell/docker-compose.yaml"
+err() {
+    echo -e "${RED}Error: $*${RESET}" >&2
+}
 
-docker compose -f "$PATH_TO_DOCKERFILE" up ubuntu --build -d
-echo -e "\n${GREEN}Ubuntu 24.04 docker container is started${RESET}"
+PATH_TO_DOCKERFILE="$HOME/VScProjects/BMSTU/BMSTU-Shell/docker-compose.yaml"
+UBUNTU_CONTAINER_NAME="ubuntu24"
+
+if ! docker compose -f "$PATH_TO_DOCKERFILE" up "$UBUNTU_CONTAINER_NAME" --build -d; then
+    err "Failed to start the docker container"
+    exit 1
+fi
+
+echo -e "\n${GREEN}Ubuntu 24 docker container is started${RESET}"
